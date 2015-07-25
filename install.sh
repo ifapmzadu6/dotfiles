7f6a17d2d4f8ccf752a3556e6e66faf8e7f27519
ifapmzadu6 <ifapmzadu6@gmail.com>
2015-07-26 07:14:15 +0900

    update

--- install.sh
+++ install.sh
@@ -7,19 +7,13 @@ cd ~/
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 
-
-# vim
-mkdir ~/.vim
-ln -sf ~/dotfiles/colors ~/.vim/colors
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+# git
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 # install neobundle
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh ./install.sh
 rm install.sh
-
-# git
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 # install git-completion.bash
 wget -nv https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
@@ -28,4 +22,9 @@ mv -f git-completion.bash .git-completion.bash
 # install git-prompt.sh
 wget -nv https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh
 mv -f git-prompt.sh .git-prompt.sh
+
+# vim
+mkdir ~/.vim
+ln -sf ~/dotfiles/colors ~/.vim/colors
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
