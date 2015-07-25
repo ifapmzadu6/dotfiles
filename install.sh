da9a2eb30ade585780ce08891a11770355384dab
ifapmzadu6 <ifapmzadu6@gmail.com>
2015-07-25 22:51:19 +0900

    add gitconfig

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
+
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/ftplugin ~/.vim
 ln -sf ~/dotfiles/autoload ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
