039341e0568409cca155808f0005890565de3b0a
ifapmzadu6 <ifapmzadu6@gmail.com>
2019-02-02 06:10:01 +0000

    remove unnecessary file

--- install.sh
+++ install.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# move Home Directory
-cd ~
-
-# git
+# gitconfig
 unlink ~/.gitconfig
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
-# vim
+# vimrc
 unlink ~/.vimrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
-# vim
+# vim plugin
 rm -rf ~/.vim
 mkdir -p ~/.vim/pack/mypackage/start
 cd ~/.vim/pack/mypackage/start
-
-# vim plugin
 git clone https://github.com/w0ng/vim-hybrid.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/tpope/vim-fugitive.git
