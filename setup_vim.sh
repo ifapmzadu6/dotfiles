0b6b9e047ba5d96257f7ad90e8dc7bff4fc16e93
ifapmzadu6 <ifapmzadu6@gmail.com>
2024-01-06 10:10:34 +0900

    remove vim-polyglot

--- setup_vim.sh
+++ setup_vim.sh
@@ -12,6 +12,5 @@ git clone https://github.com/w0ng/vim-hybrid.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/airblade/vim-gitgutter.git
 git clone https://github.com/itchyny/lightline.vim.git
-git clone https://github.com/sheerun/vim-polyglot.git
 git clone https://github.com/editorconfig/editorconfig-vim.git
 
