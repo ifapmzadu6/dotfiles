591319b37fe002b3671a6036a00a5d12b4465e5d
ifapmzadu6 <ifapmzadu6@gmail.com>
2026-01-04 22:14:53 +0900

    fix check existing git config

--- setup_gitconfig.sh
+++ setup_gitconfig.sh
@@ -1,56 +1,55 @@
 #!/bin/bash
 
 # alias.st
-git config alias.st > /dev/null
+git config --global --get alias.st > /dev/null
 if [ $? != 0 ]; then
     git config --global alias.st status
 fi
 
 # alias.co
-git config alias.co > /dev/null
+git config --global --get alias.co > /dev/null
 if [ $? != 0 ]; then
     git config --global alias.co checkout
 fi
 
 # alias.cm
-git config alias.cm > /dev/null
+git config --global --get alias.cm > /dev/null
 if [ $? != 0 ]; then
     git config --global alias.cm commit
 fi
 
 # alias.br
-git config alias.br > /dev/null
+git config --global --get alias.br > /dev/null
 if [ $? != 0 ]; then
     git config --global alias.br branch
 fi
 
 # alias.cp
-git config alias.cp > /dev/null
+git config --global --get alias.cp > /dev/null
 if [ $? != 0 ]; then
     git config --global alias.cp cherry-pick
 fi
 
 # alias.pl
-git config alias.pl > /dev/null
+git config --global --get alias.pl > /dev/null
 if [ $? != 0 ]; then
     git config --global alias.pl pull
 fi
 
 # alias.l
-git config alias.l > /dev/null
+git config --global --get alias.l > /dev/null
 if [ $? != 0 ]; then
     git config --global alias.l "log --format='%C(yellow)%h %C(red) %cd %C(green) %cn  %Creset%s' --date=format:'%Y/%m/%d %H:%M:%S'"
 fi
 
 # color.ui
-git config color.ui > /dev/null
+git config --global --get color.ui > /dev/null
 if [ $? != 0 ]; then
     git config --global color.ui auto
 fi
 
 # core.editor
-git config core.editor > /dev/null
+git config --global --get core.editor > /dev/null
 if [ $? != 0 ]; then
     git config --global core.editor vim
 fi
-
